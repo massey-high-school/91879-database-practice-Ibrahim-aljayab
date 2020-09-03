e61384f68d4000b4f401d52ceab336186feea925
Ibrahim-aljayab <60583427+Ibrahim-aljayab@users.noreply.github.com>
2020-09-04 10:55:28 +1200

    commitment

---
+++ bottombit.php
@@ -0,0 +1,8 @@
+        <div class="box footer">
+            CC Ibrahim 2020
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>        
+        
--- index.php
+++ index.php
@@ -1,65 +1,6 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Book Review Database">
-    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews ">
-    <meta name="author" content="Ibrahim aljayab">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Nexus Book Reviews</title>
-    
-    <!-- Edit the link below / replace with your chosen google font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
-    
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="css/bookstyle.css"> 
-    
-</head>
-    
-<body>
-    
-    <div class="wrapper">
-    
-
-        
-        <div class="box banner">
-            
-        <!-- logo image linking to home page goes here -->
-        <a href="index.php">
-            <div class="box logo"  title="Logo - Click here to go to the Home Page">
-            <img class="img-circle" src="images/gen_logo.png" width="150" height="150" alt="generic logo" />
-            
-            </div>    <!-- / logo -->
-        </a>
-            
-            <h1>Orichid Reading</h1>
-        </div>    <!-- / banner -->
-
-        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
-        <div class="box nav">
-            <a href="index.html">Home</a> | 
-            <a href="page1.html">Page 1</a> | 
-            <a href="page2.html">Page 2</a> | 
-            <a href="page3.html">Page 3</a> | 
-            <a href="contact.html">Contact</a>
-        </div>    <!-- / nav -->        
-        
-        <div class="box side">
-        
-        <h2>Side Bar</h2>
-            <p>items go here</p>
-            
-            <ul>
-                <li>Here</li>
-                <li> is</li>
-            </ul>
-            
-            
-        </div> <!-- / side bar -->
-        
+<?php
+    include "topbit.php";
+?>
         <div class="box main">
             <h2>First Page heading</h2>
             
@@ -78,10 +19,8 @@
         </div>    <!-- / main -->
         
 
-        <div class="box footer">
-            CC yourname 20XX
-        </div>    <!-- / footer -->
-    
-    </div>  <!-- / wrapper  -->
-    
-</body>        
+
+
+<?php
+    include "bottombit.php";
+?>
