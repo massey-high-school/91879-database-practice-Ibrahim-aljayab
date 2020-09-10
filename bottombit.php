e6cfe0a3955f91e351a0cdff999fed572c4c01dc
Ibrahim-aljayab <60583427+Ibrahim-aljayab@users.noreply.github.com>
2020-09-11 10:47:14 +1200

    added show all
    (((((((((((((((((((((((((:

--- bottombit.php
+++ bottombit.php
@@ -1,4 +1,20 @@
-        <div class="box footer">
+        <div class="box side">
+            
+            <h3>Search | <a class="side" href="show_all.php">Show All</a></h3>
+            
+            <i>Type part of the title / author name and then search</i>
+            
+            <hr />
+            
+            Title Search <br />
+            Author Search <br />
+            Genre Search <br />
+            Rating Search
+            
+            
+</div> <!-- / side bar -->            
+            
+<div class="box footer">
             CC Ibrahim 2020
         </div>    <!-- / footer -->
     
