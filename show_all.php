f47d57b7bfa9852b59dd98fe3aa1fc29a6183839
Ibrahim-aljayab <60583427+Ibrahim-aljayab@users.noreply.github.com>
2020-09-14 09:44:26 +1200

    3:27 in the show all part 2 video
    (((((((((((((((:

--- show_all.php
+++ show_all.php
@@ -1,13 +1,47 @@
 <?php
-    include "topbit.php";
+    include "head_nav.php";
+
+$showall_sql="SELECT *
+FROM `91879_book_reviews`
+ORDER BY `91879_book_reviews`.`Title` ASC";
+$showall_query=mysqli_query($dbconnect, $showall_sql);
+$showall_rs=mysqli_fetch_assoc($showall_query);
+$count=mysqli_num_rows($showall_query);
+
 ?>
         <div class="box main">
             <h2>All Items</h2>
             
-            <!-- Results go here -->
-            <div class="results">
+            <?php
             
-            <p>Title: <span class="sub_heading">title holder</span>
+            // check if there are any results
+            
+            if ($count<1)
+                
+            {
+                
+            ?>
+            
+            <div class="error">
+                Sorry! There are no results that match your search.
+                Please use the search box in the sidebar to try again.
+                 
+            </div>
+            
+            <?php
+                
+            } // end count 'if'
+            
+            // if there are not results, output an error
+            else {
+                
+                do {
+                    
+                ?>
+            
+                        <div class="results">
+            
+            <p>Title: <span class="sub_heading"><?php echo $showall_rs['Title']; ?></span>
                 </p>
             
             <p>Author: <span class="sub_heading">author holder</span>
@@ -25,8 +59,17 @@
                 Review Placeholder
             </p>
             
+            <?php
+                    
+                } // end of 'do'
+                
+                while(showall_rs=mysqli_fetch_assoc($showall_query));
+                
+            } // and else
             
-        </div>
+            // if there are results, display them
+            
+            ?>
             
             
         </div>    <!-- / main -->
